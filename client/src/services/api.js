e72264394bf3ad072d4423590295dab63f9066d4
Rahul-shastri67 <shastrirahul246@gmail.com>
2026-06-27 15:06:48 +0530

    Connect frontend to Render API

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,24 +1,28 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
-  headers: { 'Content-Type': 'application/json' },
+  baseURL: import.meta.env.VITE_API_URL + '/api',
+  headers: {
+    'Content-Type': 'application/json',
+  },
 })
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('rp_token')
-  if (token) config.headers.Authorization = `Bearer ${token}`
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
   return config
 })
 
 api.interceptors.response.use(
-  (res) => res,
-  (err) => {
-    if (err.response?.status === 401) {
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
       localStorage.removeItem('rp_token')
       localStorage.removeItem('rp_driver')
     }
-    return Promise.reject(err)
+    return Promise.reject(error)
   }
 )
 
